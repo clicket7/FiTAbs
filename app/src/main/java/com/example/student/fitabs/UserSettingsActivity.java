bda8b8bb8556dad91a24cdec4609189032f077f3
Arturs Audris <artursaudr@gmail.com>
2017-07-11 15:32:26 +0300

    UserSettingsActivity changed, DBHandler changed

--- app/src/main/java/com/example/student/fitabs/UserSettingsActivity.java
+++ app/src/main/java/com/example/student/fitabs/UserSettingsActivity.java
@@ -50,7 +50,7 @@ public class UserSettingsActivity extends AppCompatActivity {
             SQLiteDatabase database = dbHandler.getWritableDatabase();
 
             ContentValues contentValues = new ContentValues();
-            
+
 
 
             contentValues.put(DBHandler.KEY_USERNAME, user.getName());
