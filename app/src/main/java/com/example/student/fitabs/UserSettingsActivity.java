98b532841dbbc953abda458e67fddae69c43630e
Arturs Audris <artursaudr@gmail.com>
2017-07-11 14:38:14 +0300

    DBHandler added

--- app/src/main/java/com/example/student/fitabs/UserSettingsActivity.java
+++ app/src/main/java/com/example/student/fitabs/UserSettingsActivity.java
@@ -22,8 +22,6 @@ public class UserSettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
 
-        
-
         editUsername = (EditText) findViewById(R.id.editUsername);
         editNumber = (EditText) findViewById(R.id.editTelNumber);
         checkStatus = (CheckBox) findViewById(R.id.checkBoxStatus);
