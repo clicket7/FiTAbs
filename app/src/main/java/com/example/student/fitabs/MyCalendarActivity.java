eb0a911481222b2e2a19a69f73a53c1de682cec3
Andis Krebss <andis.krebss@example.com>
2017-07-11 14:24:42 +0300

    commit calendar +

---
+++ app/src/main/java/com/example/student/fitabs/MyCalendarActivity.java
@@ -0,0 +1,13 @@
+package com.example.student.fitabs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyCalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_calendar);
+    }
+}
