bbea00084bdd26914b0cf1c7692d32cf5378949b
clicket7 <clicket7@hotmail.com>
2017-07-11 15:37:45 +0300

    Deleted buttons back from activity_chat and activity_contacts

--- app/src/main/java/com/example/student/fitabs/ChatActivity.java
+++ app/src/main/java/com/example/student/fitabs/ChatActivity.java
@@ -1,15 +1,8 @@
 package com.example.student.fitabs;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.WindowManager;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -21,66 +14,25 @@ import java.util.List;
  */
 
 public class ChatActivity extends AppCompatActivity {
-    private ArrayList<String> chatMessages = new ArrayList<>();
-    ListView chatWindow;
-    EditText editMessage;
-    ChatMessage message = new ChatMessage();
+    private String msg;
+    private List<ChatMessage> chatMessages;
+    private ListView chatWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat); // initialization of variables and creating activity view
-        editMessage = (EditText) findViewById(R.id.editMessage);
+        chatMessages = new ArrayList<ChatMessage>();
         chatWindow = (ListView) findViewById(R.id.chat);
-
-
-        //Define bottom navigation view (thats why design library in gradle was imported)
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById (R.id.bottom_navigation);
-
-        //Display right icon
-        bottomNavigationView.getMenu().getItem(1).setChecked(true);
-
-        //Define Bottom navigation view listener
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-            //Selected icon(item) - changes to the appropriate view
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId())
-                {
-                    //Contacts
-                    case R.id.action_contacts:
-                        startActivity(new Intent(ChatActivity.this, ContactsActivity.class));
-                        break;
-
-                    //Chat
-                    case R.id.action_chat:
-                        break;
-
-                    //Calendar
-                    case R.id.action_calendar:
-                        startActivity(new Intent(ChatActivity.this, MyCalendarActivity.class));
-                        break;
-
-                    //Settings
-                    case R.id.action_settings:
-                        startActivity(new Intent(ChatActivity.this, UserSettingsActivity.class));
-                        break;
-                }
-                return true;
-            }
-        });
+        setContentView(R.layout.activity_chat); // initialization of variables and creating activity view
     }
 
-    public void sendMsg(View view) {
-        String msg = editMessage.getText().toString();
-        message.setAuthor("User");
-        message.setMsg(msg);
-        chatMessages.add(message.getAuthor() + ": " + message.getMsg()); // adding message to List of ChatMessage
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, chatMessages);
+    public void sendMsg() {
+        msg = findViewById(R.id.editText).toString();
+        ChatMessage message = new ChatMessage("Kirils", msg); // creating message object
+        chatMessages.add(message); // adding message to List of ChatMessage
+        ArrayAdapter<ChatMessage> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, chatMessages);
         // creating adapter for adding all messages to chat window
         chatWindow.setAdapter(adapter); // adding messages to chat window through adapter
-        editMessage.setText("");
+        
     }
 }
--- app/src/main/java/com/example/student/fitabs/ContactsActivity.java
+++ app/src/main/java/com/example/student/fitabs/ContactsActivity.java
@@ -1,21 +1,12 @@
 package com.example.student.fitabs;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ContactsActivity extends AppCompatActivity {
-
     private List<User> contacts;
 
 
@@ -24,57 +15,9 @@ public class ContactsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         contacts = new ArrayList<User>();
         setContentView(R.layout.activity_contacts);
-
-
-        //Define bottom navigation view (thats why design library in gradle was imported)
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById (R.id.bottom_navigation);
-
-        //Display right icon
-        bottomNavigationView.getMenu().getItem(0).setChecked(true);
-
-        //Define Bottom navigation view listener
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-            //Selected icon(item) - changes to the appropriate view
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId())
-                {
-                    //Contacts
-                    case R.id.action_contacts:
-                        break;
-
-                    //Chat
-                    case R.id.action_chat:
-                        startActivity(new Intent(ContactsActivity.this, ChatActivity.class));
-                        break;
-
-                    //Calendar
-                    case R.id.action_calendar:
-                        startActivity(new Intent(ContactsActivity.this, MyCalendarActivity.class));
-                        break;
-
-                    //Settings
-                    case R.id.action_settings:
-                        startActivity(new Intent(ContactsActivity.this, UserSettingsActivity.class));
-                        break;
-                }
-                return true;
-            }
-        });
     }
 
     public void readFromDatabase() {
     // function for reading table with all contacts
-    }
-
-    public void addContact(View view) {
-        Intent intent = new Intent(ContactsActivity.this, AddContactActivity.class);
-        startActivity(intent);
-    }
-
-    public void deleteContact(View view) {
-        Toast toast = Toast.makeText(getApplicationContext(), "It's not finished yet!", Toast.LENGTH_LONG);
-        toast.show();
     }
 }
