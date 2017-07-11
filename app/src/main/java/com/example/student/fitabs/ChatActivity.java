308ed3a838f96bedaeab2e2c021a74b25cc44897
clicket7 <clicket7@hotmail.com>
2017-07-11 16:34:26 +0300

    ChatActivity trying to resolve NullPointerException

--- app/src/main/java/com/example/student/fitabs/ChatActivity.java
+++ app/src/main/java/com/example/student/fitabs/ChatActivity.java
@@ -3,6 +3,7 @@ package com.example.student.fitabs;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,22 +18,23 @@ public class ChatActivity extends AppCompatActivity {
     private String msg;
     private List<ChatMessage> chatMessages;
     private ListView chatWindow;
+    private EditText msgField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         chatMessages = new ArrayList<ChatMessage>();
         chatWindow = (ListView) findViewById(R.id.chat);
+        msgField = (EditText) findViewById(R.id.editText);
         setContentView(R.layout.activity_chat); // initialization of variables and creating activity view
     }
 
     public void sendMsg() {
-        msg = findViewById(R.id.editText).toString();
+        msg = msgField.getText().toString();
         ChatMessage message = new ChatMessage("Kirils", msg); // creating message object
         chatMessages.add(message); // adding message to List of ChatMessage
         ArrayAdapter<ChatMessage> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, chatMessages);
         // creating adapter for adding all messages to chat window
         chatWindow.setAdapter(adapter); // adding messages to chat window through adapter
-        
     }
 }
