781e80460e97318fd4b0bc95326898a34bdb12d5
Karina Boikova <karinaboikova@gmail.com>
2017-07-11 10:47:00 +0300

    Changes in UserSettings activity...

--- app/src/main/java/com/example/student/fitabs/UserSettingsActivity.java
+++ app/src/main/java/com/example/student/fitabs/UserSettingsActivity.java
@@ -3,21 +3,37 @@ package com.example.student.fitabs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class UserSettingsActivity extends AppCompatActivity {
+
+    EditText editUsername, editNumber;
+    CheckBox checkStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
 
+        editUsername = (EditText) findViewById(R.id.editUsername);
+        editNumber = (EditText) findViewById(R.id.editTelNumber);
+        checkStatus = (CheckBox) findViewById(R.id.checkBoxStatus);
         User user = new User();
+
+        editUsername.setText(user.getName());
+        editNumber.setText(user.getTelnumber());
+        checkStatus.setChecked(user.getStatus());
     }
 
     public void saveUser(View view) {
-
+        User user = new User();
+        user.setName(editUsername.getText().toString());
+        user.setTelnumber(editNumber.getText().toString());
+        user.setStatus(checkStatus.isChecked());
     }
 
     public void closeUserSettings(View view) {
+        finish();
     }
 }
