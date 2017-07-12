1a4c5cc1f5fe0f2d0ec6eab799cb1c648eecfdcb
oskarsmuraska <oskarsmuraska@inbox.lv>
2017-07-12 14:49:41 +0300

    Updated design

--- app/src/main/java/com/example/student/fitabs/UserSettingsActivity.java
+++ app/src/main/java/com/example/student/fitabs/UserSettingsActivity.java
@@ -7,14 +7,12 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import static android.R.attr.name;
 
 public class UserSettingsActivity extends AppCompatActivity {
 
