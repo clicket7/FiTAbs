9543da84974f13b6270dd49b6098b25c70c3cf8e
oskarsmuraska <oskarsmuraska@inbox.lv>
2017-07-12 10:38:07 +0300

    FitAbs - Menu (working and no old activities)

--- app/src/main/java/com/example/student/fitabs/ContactsActivity.java
+++ app/src/main/java/com/example/student/fitabs/ContactsActivity.java
@@ -1,12 +1,19 @@
 package com.example.student.fitabs;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ContactsActivity extends AppCompatActivity {
+
     private List<User> contacts;
 
 
@@ -15,6 +22,41 @@ public class ContactsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         contacts = new ArrayList<User>();
         setContentView(R.layout.activity_contacts);
+
+
+        //Define bottom navigation view (thats why design library in gradle was imported)
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById (R.id.bottom_navigation);
+
+        //Define Bottom navigation view listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            //Selected icon(item) - changes to the appropriate view
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    //Contacts
+                    case R.id.action_contacts:
+                        break;
+
+                    //Chat
+                    case R.id.action_chat:
+                        startActivity(new Intent(ContactsActivity.this, ChatActivity.class));
+                        break;
+
+                    //Calendar
+                    case R.id.action_calendar:
+                        startActivity(new Intent(ContactsActivity.this, MyCalendarActivity.class));
+                        break;
+
+                    //Settings
+                    case R.id.action_settings:
+                        startActivity(new Intent(ContactsActivity.this, UserSettingsActivity.class));
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     public void readFromDatabase() {
