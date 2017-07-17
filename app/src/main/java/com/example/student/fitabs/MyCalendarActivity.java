1a5fccec97de87d9265079a1bd1a1ed5a225578b
Igors Dola <idagader@gmail.com>
2017-07-17 12:46:43 +0300

    working calendar , no save option

--- app/src/main/java/com/example/student/fitabs/MyCalendarActivity.java
+++ app/src/main/java/com/example/student/fitabs/MyCalendarActivity.java
@@ -1,18 +1,23 @@
 package com.example.student.fitabs;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 
-import com.example.student.fitabs.Day;
-import com.example.student.fitabs.R;
 
 import java.util.ArrayList;
+
+
+import static com.example.student.fitabs.R.id.calendarView;
 
 public class MyCalendarActivity extends AppCompatActivity implements View.OnClickListener{
     CalendarView _Calendar = null;
@@ -32,12 +37,10 @@ public class MyCalendarActivity extends AppCompatActivity implements View.OnClic
         _Schedule = fh.deserializeObject();
 
         //Setup widgets for easier access
-        _Calendar = (CalendarView) this.findViewById(R.id.calendarView);
+        _Calendar = (CalendarView) this.findViewById(calendarView);
         _ButtonSave = (Button) this.findViewById(R.id.button8);
         _EditText = (EditText) this.findViewById(R.id.editText);
 
-        //TODO update _SelectedDay so app doesn't crash if a choice is made before changing
-        //the date
 
         //Set up button
         _ButtonSave.setText(R.string.btn_edit);
@@ -50,10 +53,47 @@ public class MyCalendarActivity extends AppCompatActivity implements View.OnClic
                 selectedDateChanged(view, year, month, dayOfMonth);
             }
 
-
-
-
         });
+/*
+
+        setContentView(R.layout.activity_main);
+        //Define bottom navigation view (thats why design library in gradle was imported)
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById (R.id.bottom_navigation);
+
+        //Display right icon
+        bottomNavigationView.getMenu().getItem(2).setChecked(true);
+
+        //Define Bottom navigation view listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            //Selected icon(item) - changes to the appropriate view
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    //Contacts
+                    case R.id.action_contacts:
+                        startActivity(new Intent(MyCalendarActivity.this, ContactsActivity.class));
+                        break;
+
+                    //Chat
+                    case R.id.action_chat:
+                        startActivity(new Intent(MyCalendarActivity.this, ChatActivity.class));
+                        break;
+
+                    //Calendar
+                    case R.id.action_calendar:
+                        break;
+
+                    //Settings
+                    case R.id.action_settings:
+                        startActivity(new Intent(MyCalendarActivity.this, UserSettingsActivity.class));
+                        break;
+                }
+                return true;
+            }
+        });
+*/
 
     }
 
