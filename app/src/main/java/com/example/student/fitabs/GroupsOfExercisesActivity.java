b2537aee4fff332de9a213a535f8b9575427f4f5
clicket7 <clicket7@hotmail.com>
2017-07-18 10:13:26 +0300

    Added new activities about Exercises

---
+++ app/src/main/java/com/example/student/fitabs/GroupsOfExercisesActivity.java
@@ -0,0 +1,18 @@
+package com.example.student.fitabs;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by student on 17.18.7.
+ */
+
+public class GroupsOfExercisesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groups_of_exercises);
+    }
+
+}
