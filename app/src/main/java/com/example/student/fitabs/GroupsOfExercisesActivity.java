84f7278ea1ccb1884a0fb5f3162be44ae7c90b7c
clicket7 <clicket7@hotmail.com>
2017-07-18 10:18:43 +0300

    Added menu tray into GroupsOfExercisesActivity

--- app/src/main/java/com/example/student/fitabs/GroupsOfExercisesActivity.java
+++ app/src/main/java/com/example/student/fitabs/GroupsOfExercisesActivity.java
@@ -1,7 +1,11 @@
 package com.example.student.fitabs;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 /**
  * Created by student on 17.18.7.
@@ -13,6 +17,36 @@ public class GroupsOfExercisesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups_of_exercises);
+
+        //Define bottom navigation view (thats why design library in gradle was imported)
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        //Display right icon
+        bottomNavigationView.getMenu().getItem(0).setChecked(true);
+        //Define Bottom navigation view listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            //Selected icon(item) - changes to the appropriate view
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    //Contacts
+                    case R.id.action_contacts:
+                        startActivity(new Intent(GroupsOfExercisesActivity.this, ContactsActivity.class));
+                        break;
+                    //Chat
+                    case R.id.action_chat:
+                        break;
+                    //Calendar
+                    case R.id.action_calendar:
+                        startActivity(new Intent(GroupsOfExercisesActivity.this, MyCalendarActivity.class));
+                        break;
+                    //Settings
+                    case R.id.action_settings:
+                        startActivity(new Intent(GroupsOfExercisesActivity.this, UserSettingsActivity.class));
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
 }
