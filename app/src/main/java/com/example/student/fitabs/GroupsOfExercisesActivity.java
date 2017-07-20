a404e0dc894137df66ff7b383bf4dc766424b379
Karina Boikova <karinaboikova@gmail.com>
2017-07-20 14:44:31 +0300

    Now exercises must work faster... But I'm not sure...

--- app/src/main/java/com/example/student/fitabs/GroupsOfExercisesActivity.java
+++ app/src/main/java/com/example/student/fitabs/GroupsOfExercisesActivity.java
@@ -101,36 +101,38 @@ public class GroupsOfExercisesActivity extends AppCompatActivity {
 
     public void readFromDatabase() {
 
-        dbHandler.deleteAllExercises();
+        SQLiteDatabase db = dbHandler.getReadableDatabase();
+        Cursor cursor = db.query(DBHandler.TABLE_EXERCISES,
+                null, null, null, null, null, null);
+        if (!cursor.moveToFirst()) {
+            dbHandler.addExercise("Chest", "Decline Push-Up", "Move your feet up to a box or bench. Just do push-ups.", "pushup");
+            dbHandler.addExercise("Chest", "Chain Press", "Lower the chains by flexing the elbows, unloading some of the chain onto the floor.", "chainpress");
+            dbHandler.addExercise("Chest", "ButterFly", "Sit on the machine with your back flat on the pad.Push the handles together slowly.", "butterfly");
 
-        dbHandler.addExercise("Chest", "Decline Push-Up", "Move your feet up to a box or bench. Just do push-ups.", "pushup");
-        dbHandler.addExercise("Chest", "Chain Press", "Lower the chains by flexing the elbows, unloading some of the chain onto the floor.", "chainpress");
-        dbHandler.addExercise("Chest", "ButterFly", "Sit on the machine with your back flat on the pad.Push the handles together slowly.", "butterfly");
+            dbHandler.addExercise("Arms", "Barbell Curl ", "Stand up with your torso upright while holding a barbell at a shoulder-width grip.", "burbellcurl");
+            dbHandler.addExercise("Arms", "Bench Dips", "Slowly lower your body as you inhale by bending at the elbows until you lower yourself far enough.", "benchdips");
+            dbHandler.addExercise("Arms", "Body-Up", "Slowly lower your forearms back to the ground by allowing the elbows to flex. Repeat for the desired number of repetitions.", "bodyup");
 
-        dbHandler.addExercise("Arms", "Barbell Curl ", "Stand up with your torso upright while holding a barbell at a shoulder-width grip.", "burbellcurl");
-        dbHandler.addExercise("Arms", "Bench Dips", "Slowly lower your body as you inhale by bending at the elbows until you lower yourself far enough.", "benchdips");
-        dbHandler.addExercise("Arms", "Body-Up", "Slowly lower your forearms back to the ground by allowing the elbows to flex. Repeat for the desired number of repetitions.", "bodyup");
+            dbHandler.addExercise("Legs", "Bench Jump", "Jump over the bench, landing with the knees bent, absorbing the impact through the legs.", "benchjump");
+            dbHandler.addExercise("Legs", "Bicycling", "To begin, seat yourself on the bike and adjust the seat to your height.", "bicycling");
+            dbHandler.addExercise("Legs", "Bodyweight Squat", "Begin the movement by flexing your knees and hips, sitting back with your hips.", "bodyweightsquat");
 
-        dbHandler.addExercise("Legs", "Bench Jump", "Jump over the bench, landing with the knees bent, absorbing the impact through the legs.", "benchjump");
-        dbHandler.addExercise("Legs", "Bicycling", "To begin, seat yourself on the bike and adjust the seat to your height.", "bicycling");
-        dbHandler.addExercise("Legs", "Bodyweight Squat", "Begin the movement by flexing your knees and hips, sitting back with your hips.", "bodyweightsquat");
-
-        dbHandler.addExercise("Abs", "Sit-Up", "Flex your hips and spine to raise your torso toward your knees.", "situp");
-        dbHandler.addExercise("Abs", "Air Bike", "Slowly go through a cycle pedal motion kicking forward with the right leg and bringing in the knee of the left leg. Bring your right elbow close to your left knee by crunching to the side, as you breathe out.", "airbike");
-        dbHandler.addExercise("Abs", "Dead Bug", "Begin lying on your back with your hands extended above you toward the ceiling. Bring your feet, knees, and hips up to 90 degrees.", "deadbug");
-
+            dbHandler.addExercise("Abs", "Sit-Up", "Flex your hips and spine to raise your torso toward your knees.", "situp");
+            dbHandler.addExercise("Abs", "Air Bike", "Slowly go through a cycle pedal motion kicking forward with the right leg and bringing in the knee of the left leg. Bring your right elbow close to your left knee by crunching to the side, as you breathe out.", "airbike");
+            dbHandler.addExercise("Abs", "Dead Bug", "Begin lying on your back with your hands extended above you toward the ceiling. Bring your feet, knees, and hips up to 90 degrees.", "deadbug");
+        }
 
         String selectQuery = "SELECT DISTINCT " + dbHandler.KEY_EX_TYPE + " FROM " + dbHandler.TABLE_EXERCISES;
-        SQLiteDatabase db = dbHandler.getReadableDatabase();
-        Cursor cursor = db.rawQuery(selectQuery, null);
+        Cursor c = db.rawQuery(selectQuery, null);
         // looping through all rows and adding to list
-        if (cursor.moveToFirst()) {
+        if (c.moveToFirst()) {
             do {
-                groups.add(cursor.getString(0));
-            } while (cursor.moveToNext());
+                groups.add(c.getString(0));
+            } while (c.moveToNext());
         }
 
         cursor.close();
+        c.close();
         dbHandler.close();
     }
 
